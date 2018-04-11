13c69e8768c9c32939d68cd97e08dcdfdc95d72a
mrkkrp <mark.karpov@tweag.io>
2018-04-11 14:42:56 +0700

    Fix a typo
    [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -106,7 +106,7 @@ addReport optMetadata optBuildLog optTarget optOutputDir = do
     show (failingPackages buildResults)
   putStrLn $ "  Unreachable packages: " ++
     show (unreachablePackages buildResults)
-  putStrLn $ "  Packages that build" ++
+  putStrLn $ "  Packages that build: " ++
     show (succeedingPackages buildResults)
   putStrLn $ "Extending history file " ++ historyPath
   saveHistory historyPath (extendHistory history actualItem)
